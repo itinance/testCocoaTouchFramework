db29521f1684c5b909026cc3385c3e323f80905a
Hagen Hübel <hhuebel@itinance.com>
2014-11-30 12:16:44 +0100

    add googmorning.h

--- libTestFramework/libTestFramework/libTestFramework.h
+++ libTestFramework/libTestFramework/libTestFramework.h
@@ -14,3 +14,4 @@ FOUNDATION_EXPORT const unsigned char libTestFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <libTestFramework/PublicHeader.h>
 
 
+#import "GoodMorning.h"
